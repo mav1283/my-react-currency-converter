863ef3b58b03fed052f8b550882fd34a133cecfd
Dani Paolo S. Dullano <pdullano@gmail.com>
2020-03-13 23:12:30 +0800

    Updates

---
+++ precache-manifest.d36858d2eb1b66992af2ddebeee1c5a2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ccbad859a46b36e49c6f151ef93e6467",
+    "url": "/my-react-currency-converter/index.html"
+  },
+  {
+    "revision": "34c238177d730196bb86",
+    "url": "/my-react-currency-converter/static/css/main.d5b0ee04.chunk.css"
+  },
+  {
+    "revision": "698c31ed43f893f95118",
+    "url": "/my-react-currency-converter/static/js/2.3d43fe5d.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/my-react-currency-converter/static/js/2.3d43fe5d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "34c238177d730196bb86",
+    "url": "/my-react-currency-converter/static/js/main.cf5ae83c.chunk.js"
+  },
+  {
+    "revision": "7664f931ac3e80452770",
+    "url": "/my-react-currency-converter/static/js/runtime-main.105758e8.js"
+  }
+]);
